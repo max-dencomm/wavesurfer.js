5a79c01ba1fc5c1a7c169bcac79a622e3da73024
Max <max@dencomm.ai>
2024-02-02 10:36:29 +0900

    Customize prevent-click.js

--- src/util/prevent-click.js
+++ src/util/prevent-click.js
@@ -5,8 +5,10 @@
  * @param {object} event The click event
  */
 function preventClickHandler(event) {
+    console.log("Customization worked!");
     event.stopPropagation();
-    document.body.removeEventListener('click', preventClickHandler, true);
+    document.body.removeEventListener("click", preventClickHandler, true);
+    document.body.removeEventListener("contextmenu", preventClickHandler, true);
 }
 
 /**
@@ -15,5 +17,6 @@ function preventClickHandler(event) {
  * @param {object} values Values
  */
 export default function preventClick(values) {
-    document.body.addEventListener('click', preventClickHandler, true);
+    document.body.addEventListener("click", preventClickHandler, true);
+    document.body.addEventListener("contextmenu", preventClickHandler, true);
 }
